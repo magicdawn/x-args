6ac0fa20ecfcc06cb8487aebd03bf9e841dd6982
magicdawn <magicdawn@qq.com>
2024-07-02 13:19:31 +0800

    fix: enum SessionControl should not use type export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from './util/BaseCommand'
 export * from './util/file'
 
 // manual start command
-export { startTxtCommand, type SessionControl, type TxtCommandArgs } from './commands/txt'
+export { SessionControl, startTxtCommand, type TxtCommandArgs } from './commands/txt'
